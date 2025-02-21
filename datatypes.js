08ae2a4e2b2e1435fbcd66861022bce4b6ac59de
Christian-Hermann <christianghermann@gmail.com>
2025-02-21 15:39:47 -0600

    work

--- datatypes.js
+++ datatypes.js
@@ -1,10 +1,12 @@
 /**
  * DATATYPES:
  * 
- * 0: 
+ * 0: Datatypes in JavaScript classify and define the kind of values a variable can hold 
+ * and the operations that can be performed on it, each value has a data type, defining
+ * its nature.  
  * 
  * 
- * 
+ * There are two main categories of datatypes: primitive and non-primitive.
  * 
  * 
  * 
@@ -12,27 +14,33 @@
 
 
 // 1. Number // 
+   Example: 1, -1, 500, 3.14, 
 
 
+// 2. String // 
+   Example: "Christian", "Hermann"
 
-// 2. String //
 
-
-
-// 3. Boolean //
-
+// 3. Boolean // 
+   Example: true, false
 
 
 // 4. Array //
-
+    Example: [1, 2, 3, 4]
 
 
 // 5. Object //
-
-
+const person = {
+    firstName: "Christian",
+    lastName: "Hermann",
+    age: 40
+}
+    
 
 // 6. Function //
-
+function greet() {                  // Function keyword declares greet 
+    console.log("Hello, World!");   // When function is invoked - prints to the console
+}
 
 
 // 7. Undefined //
@@ -46,7 +54,8 @@ console.log(myVar); // Output: null
 
 
 // 9. NaN //
-
+const result = 0 / 0;
+console.log(result);     // outputs: NaN
 
 
 // 10. Infinity && -Infinity //
