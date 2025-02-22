ec102a5ea9d2e9138abe00ce32271ce616ce2d4a
Christian-Hermann <christianghermann@gmail.com>
2025-02-22 10:19:15 -0600

    work

--- datatypes.js
+++ datatypes.js
@@ -6,9 +6,10 @@
  * its nature.  
  * 
  * 
- * There are two main categories of datatypes: primitive and non-primitive.
- * 
- * 
+ * There are two main categories of datatypes: primitive and complex.
+ *
+ * Primitive include: string, number, boolean, undefined, NaN
+ * Complex include: array, function, object
  * 
  */
 
@@ -38,7 +39,7 @@ const person = {
     
 
 // 6. Function //
-function greet() {                  // Function keyword declares greet 
+function greet() {   // Function keyword declares greet 
     console.log("Hello, World!");   // When function is invoked - prints to the console
 }
 
@@ -59,12 +60,55 @@ console.log(result);     // outputs: NaN
 
 
 // 10. Infinity && -Infinity //
+// Infintiy and -Infinity are part if the Number datatype, it is a global property that represents a
+// value greater than any numerical value
 
+const positiveInfinity = Infinity;
+console.log(positiveInfinity);       // Outputs: Infinity
+
+const negativeInfinity = -Infinity;
+console.log(negativeInfinity);       // Outputs: -Infinity
 
 
 // 11. What is the difference between primitive/simple and complex data types
 
+// Primitiva data types are fundemantal bulding blocks, representing basic units of information. Complex
+// data types are constructed from primitive types, allowing more intricate data structures. Primitive date 
+// types are immutable and stoe a single value directly in memory. Complex data types are mutable and store
+// collections of values, holding refernces to memory locations.
 
 
 // 12. Primitive values ae passed to a function BY COPY, complex values are BY REFERENCE. What does that mean, 
-// and how are they different
+// and how are they different.
+
+// When you pass a value to a function, the way that it can be handled depends on whehter it is a primitve 
+// or complex datatype.
+
+// When you pass a primitive value to function a copy is made. This means that any changes made to the parameter 
+// inside the function do not affect the original variable outside the function.
+
+function modifyValue(n) {
+    n = 5;
+    console.log(n);     // Outputs: 5
+  }
+
+  let x = 10;
+  modifyValue(x);
+  console.log(x);       // Outputs: 10
+
+// In this example, x remains 10 after the function call because n inside the function is a copy of x
+
+
+// When you pass a non-primitive value to a function, a refernce to the original is passed. So, changes
+// made to the paramter inside the function can affect original balue outside of the function
+
+function modifyObject(obj) {
+    obj.name = 'Christian';
+    console.log(obj);                // Outputs: { name: 'Christian' }
+  }
+  
+  let person = { name: 'Lisa' };
+  modifyObject(person);
+  console.log(person);               // Outputs: { name: 'Christian' }
+
+// In this example person is modified because obj inside the fuction references the same object as person
